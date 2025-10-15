89f855dcab958ea2926944778f14ed43845698a8
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2025-10-16 00:25:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+
+let rotateline = document.getElementById('line');
+let rotationdegree = 0;
+
+setInterval(() =>{
+rotationdegree+=2;
+rotateline.style.transform = `rotate(${rotationdegree}deg)`;
+
+},20);
+
+
